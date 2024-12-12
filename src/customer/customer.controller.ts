e331891c5042cee349a63b904954c11836f715bb
Lucas Santos de Souza <lecvsantos@gmail.com>
2024-12-12 19:58:16 -0300

    lint

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
-import { CustomerIdDto } from "./dto/customer-id.dto";
+import { CustomerIdDto } from './dto/customer-id.dto';
 
 @Controller('customer')
 export class CustomerController {
