5a71ddaa4a995f8e8ed4d89fa5df73e9c56e1a67
Lucas Santos de Souza <lecvsantos@gmail.com>
2024-12-12 19:55:36 -0300

    validação de cliente existente e rota para buscar cliente por id

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
+import { CustomerIdDto } from "./dto/customer-id.dto";
 
 @Controller('customer')
 export class CustomerController {
@@ -9,5 +10,10 @@ export class CustomerController {
   @Post()
   store(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customerService.store(createCustomerDto);
+  }
+
+  @Get('/:customerId')
+  findById(@Param() params: CustomerIdDto) {
+    return this.customerService.findById(params.customerId);
   }
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Customer } from './interfaces/customer.interface';
@@ -11,7 +11,36 @@ export class CustomerService {
   ) {}
 
   async store(createCustomerDto: CreateCustomerDto): Promise<Customer> {
+    const { cpf, email } = createCustomerDto;
+
+    const existingCustomerCpf = await this.customerModel
+      .findOne({ cpf: cpf })
+      .exec();
+
+    if (existingCustomerCpf) {
+      throw new HttpException(`Cliente com o cpf ${cpf} já cadastrado`, 409);
+    }
+
+    const existingCustomerEmail = await this.customerModel
+      .findOne({ email: email })
+      .exec();
+
+    if (existingCustomerEmail) {
+      throw new HttpException(
+        `Cliente com o email ${email} já cadastrado`,
+        409,
+      );
+    }
+
     const createCustomer = new this.customerModel(createCustomerDto);
     return createCustomer.save();
+  }
+
+  async findById(customerId: string): Promise<Customer> {
+    const customer = await this.customerModel.findById(customerId);
+    if (!customer) {
+      throw new HttpException(`Cliente não encontrado`, 404);
+    }
+    return customer;
   }
 }
