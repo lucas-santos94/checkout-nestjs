fae6af4e2411c25b7ea6cdaedbed7062a4178aa9
Lucas Santos de Souza <lecvsantos@gmail.com>
2024-12-12 20:30:08 -0300

    criando módulo de pedido

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './customer/customer.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-sandbox'),
     ProductModule,
     CustomerModule,
+    OrderModule,
   ],
 })
 export class AppModule {}
